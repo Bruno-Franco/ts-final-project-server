b9f000e812d00107a9aba863b679ca1bcb083f50
Bruno Frasan <brunofrancco@gmail.com>
2025-01-15 13:53:05 +0000

    dot env

--- dist/server.js
+++ dist/server.js
@@ -1,7 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const app = require('./app');
-require('dotenv').config();
 // ℹ️ Sets the PORT for our app to have access to it. If no env has been set, we hard code it to 5005
 const PORT = process.env.PORT || 5005;
 app.listen(PORT, () => {
