88e08153e71e330a23e97b13c2503cb0f872f6da
Bruno Frasan <brunofrancco@gmail.com>
2025-01-15 13:42:51 +0000

    cofig add

--- src/config/index.ts
+++ src/config/index.ts
@@ -14,9 +14,9 @@ const cookieParser = require('cookie-parser')
 const cors = require('cors')
 
 const FRONTEND_URL = process.env.ORIGIN || 'http://localhost:3000'
-
+import { Express } from 'express'
 // Middleware configuration
-module.exports = (app) => {
+module.exports = (app: Express) => {
 	// Because this is a server that will accept requests from outside and it will be hosted ona server with a `proxy`, express needs to know that it should trust that setting.
 	// Services like heroku use something called a proxy and you need to add this to your server
 	app.set('trust proxy', 1)
